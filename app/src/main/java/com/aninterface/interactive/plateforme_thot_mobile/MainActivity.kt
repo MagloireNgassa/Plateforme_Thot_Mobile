bc1667c3db460290cb416365b7954dc8125a0de2
Magloire NGASSA <mag_ndosseu@outlook.com>
2021-01-26 20:11:20 -0500

    initialisation de la plateforme d'echange

---
+++ app/src/main/java/com/aninterface/interactive/plateforme_thot_mobile/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.aninterface.interactive.plateforme_thot_mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
